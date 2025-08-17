53e73cfbb080ccd9673eed8a83a3100991f88613
aya241-cloud <aya.hamdi@isimg.tn>
2025-08-17 18:58:10 +0100

    Fix Tailwind CSS configuration

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'path';
 export default defineConfig({
-  plugins: [vue(),tailwindcss()],
+  plugins: [vue(),tailwindcss(),autoprefixer(),],
   resolve: {
        alias: {
          '@': resolve(__dirname, 'src'), // This allows you to use '@' as an alias for 'src'
        },
-       tailwindcss,
-       autoprefixer,
      },
 })
